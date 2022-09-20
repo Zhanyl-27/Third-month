0b67fce835442c52604c969d12c8da3699505e01
Zhanyl-27 <108879757+Zhanyl-27@users.noreply.github.com>
2022-09-20 15:18:43 +0600

    Homework-8

---
+++ Homework-8/script.js
@@ -0,0 +1,33 @@
+const postsEl = document.getElementById("postscontent");
+const postform = document.getElementById("postform");
+
+function generatePosts(event)
+{
+    postsEl.InnerHTML = "";
+    let formData = new FormData(postform);
+    var uid = formData.get("userid");
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => data.forEach((posts) => 
+  {
+    if(posts.userId == uid)
+    {
+        let container = document.createElement("div");
+        let text = document.createElement('p');
+        let title = document.createElement('h3');
+
+        title.innerText = posts.title;
+        text.innerText = posts.body;
+
+        container.appendChild(title);
+        container.appendChild(text);
+
+        postsEl.appendChild(container);
+        
+    }
+    console.log(posts);
+  }));
+  event.preventDefault();
+}
+
+postform.addEventListener('submit', generatePosts);
